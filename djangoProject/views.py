f31d6d7c371260f0b342600fef7bfe637a641c98
aldo_ <aldoarreola69@gmail.com>
2022-11-19 13:06:17 -0600

    Video 9

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
+from django.template import loader
+from django.shortcuts import render
 
 class Persona(object):
     def __init__(self, nombre, apellido):
@@ -8,18 +10,20 @@ class Persona(object):
         self.apellido = apellido
 
 def saludo(request):
-    p1 = Persona("Profesor Juan","Barrón")
+    p1 = Persona(" Profesor Juan","Barrón")
     temas = ["Plantillas", "Modelos","Vitas","despliegue"]
     #nombre = "Juan"
     #apellido = "Arreola"
     fecha = datetime.datetime.now()
 
-    doc_externo = open(r"C:\Users\aldo_\Documents\lalo\djangoProject\plantillas\saludo.html")
-    plt = Template(doc_externo.read())
-    doc_externo.close()
-    ctx = Context({"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "fecha":fecha, "temas":temas})
-    documento = plt.render(ctx)
-    return HttpResponse(documento)
+   #doc_externo = open(r"C:\Users\aldo_\Documents\lalo\djangoProject\plantillas\saludo.html")
+    #plt = Template(doc_externo.read())
+    #doc_externo.close()
+
+    #doc_externo = loader.get_template('saludo.html')
+    #ctx = Context({"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "fecha":fecha, "temas":temas})
+    #documento = doc_externo.render({"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "fecha":fecha, "temas":temas})
+    return render(request,"saludo.html",{"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "fecha":fecha, "temas":temas})
 
 def despedida(request):
    return HttpResponse("Adios amigos del youtube")
